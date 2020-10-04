9fd2fb97520fbfdf17809a8226138394099638c6
uytv-1398 <tran.van.uy@sun-asterisk.com>
2020-10-04 19:28:28 +0700

    Add users

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
   def show
     @hello = "Hello world!"
+    @users = User.limit(10)
     # CountAccessWorker.perform_async
     # AdminMailer.send_mail_to("vanuy113@gmail.com").deliver_now
   end
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -63,7 +63,7 @@ set :branch, :develop
 set :deploy_to, '/var/www/dep-puma-app'
 set :pty, true
 set :linked_files, %w{config/database.yml config/secrets.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 set :keep_releases, 5
 set :rvm_type, :user
 set :rvm_ruby_version, 'ruby-2.5.3' 
